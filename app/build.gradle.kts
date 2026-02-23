fb4aa50315e9b46cce02454999f59b02c7e6c2e4
Paul <paul@local>
2026-02-23 00:26:16 +0000

    Set APK archive name to localproxy

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,8 @@ android {
     }
 }
 
+base.archivesName.set("localproxy")
+
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
